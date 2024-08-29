70dc9f2c5c0f06e3c7fefb83b3706ee511e60a5d
David Reeves <reevdav@gmail.com>
2024-08-29 23:23:40 +0100

    Tidy file utils

--- src/file_utils.cpp
+++ src/file_utils.cpp
@@ -8,65 +8,45 @@ namespace dr
 bool read_text_file(char const* const path, String& buffer)
 {
     std::ifstream in{path, std::ios::in};
-
-    if (in)
-    {
-        using Iter = std::istreambuf_iterator<char>;
-        buffer.assign(Iter{in}, {});
-        return true;
-    }
-    else
-    {
+    if (!in)
         return false;
-    }
+
+    using Iter = std::istreambuf_iterator<char>;
+    buffer.assign(Iter{in}, {});
+    return true;
 }
 
 bool append_text_file(char const* const path, String& buffer)
 {
     std::ifstream in{path, std::ios::in};
-
-    if (in)
-    {
-        using Iter = std::istreambuf_iterator<char>;
-        std::copy(Iter{in}, Iter{}, std::back_inserter(buffer));
-        return true;
-    }
-    else
-    {
+    if (!in)
         return false;
-    }
+
+    using Iter = std::istreambuf_iterator<char>;
+    std::copy(Iter{in}, Iter{}, std::back_inserter(buffer));
+    return true;
 }
 
 bool read_binary_file(char const* const path, DynamicArray<u8>& buffer)
 {
     std::ifstream in{path, std::ios::in | std::ios::binary};
-
-    if (in)
-    {
-        using Iter = std::istreambuf_iterator<char>;
-        buffer.assign(Iter{in}, {});
-        return true;
-    }
-    else
-    {
+    if (!in)
         return false;
-    }
+
+    using Iter = std::istreambuf_iterator<char>;
+    buffer.assign(Iter{in}, {});
+    return true;
 }
 
 bool append_binary_file(char const* const path, DynamicArray<u8>& buffer)
 {
     std::ifstream in{path, std::ios::in};
-
-    if (in)
-    {
-        using Iter = std::istreambuf_iterator<char>;
-        std::copy(Iter{in}, Iter{}, std::back_inserter(buffer));
-        return true;
-    }
-    else
-    {
+    if (!in)
         return false;
-    }
+
+    using Iter = std::istreambuf_iterator<char>;
+    std::copy(Iter{in}, Iter{}, std::back_inserter(buffer));
+    return true;
 }
 
 } // namespace dr
