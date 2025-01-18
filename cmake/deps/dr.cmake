49f6c7aa9851bf31d309a2acb740eb1bebb936be
David Reeves <reevdav@gmail.com>
2025-01-18 13:01:11 +0000

    Bump version to 0.3.0

--- cmake/deps/dr.cmake
+++ cmake/deps/dr.cmake
@@ -4,17 +4,9 @@ endif()
 
 include(FetchContent)
 
-# NOTE(dr): Pinned to specific revision until 0.4.0
-#[[
 FetchContent_Declare(
     dr
-    URL https://github.com/davreev/dr/archive/refs/tags/0.3.0.zip
-)
-]]
-FetchContent_Declare(
-    dr
-    GIT_REPOSITORY https://github.com/davreev/dr.git
-    GIT_TAG 82b866b16ce72cc6463cd5f1517b0ca0e902ab34
+    URL https://github.com/davreev/dr/archive/refs/tags/0.4.0.zip
 )
 
 FetchContent_MakeAvailable(dr)
