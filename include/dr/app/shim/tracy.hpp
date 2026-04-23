aece0e86a6894301a343d9d84a7d002a178d573a
David Reeves <reevdav@gmail.com>
2026-04-23 23:45:00 +0100

    Fix Windows build

--- include/dr/app/shim/tracy.hpp
+++ include/dr/app/shim/tracy.hpp
@@ -1,9 +1,11 @@
 #if defined(__APPLE__)
 #include <OpenGL/gl3.h>
 #elif !defined(__EMSCRIPTEN__)
+#if defined(_WIN32)
+#include <windows.h>
+#endif
 #define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
-#include <GL/glext.h>
 #endif
 
 #include <tracy/Tracy.hpp>
