c76b8e114cb8f81cf6e74b02b482c6344cb89e6a
David Reeves <reevdav@gmail.com>
2026-07-12 11:42:18 +0100

    Fix Tracy shim
    Only include platform headers when Tracy is actually enabled. Also guard windows.h include to avoid min/max macro clash.

--- include/dr/app/shim/tracy.hpp
+++ include/dr/app/shim/tracy.hpp
@@ -1,7 +1,8 @@
-#if defined(__APPLE__)
-#include <OpenGL/gl3.h>
-#elif !defined(__EMSCRIPTEN__)
+#if defined(TRACY_ENABLE) && !defined(__APPLE__) && !defined(__EMSCRIPTEN__)
 #if defined(_WIN32)
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <windows.h>
 #endif
 #define GL_GLEXT_PROTOTYPES
